9e76b10b189c04a7a69c9d69f568144f72de5ddc
pohjie <0xArlet@gmail.com>
2022-11-03 11:51:51 +0800

    Implement announcement of winner at 5 points

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,24 +24,30 @@ function getOutcome(playerSelection, computerSelection) {
         case 'Rock':
           return getTieMessage(playerSelection, computerSelection);
         case 'Paper':
+          computerScore += 1;
           return getLossMessage(playerSelection, computerSelection);
         default:
+          playerScore += 1;
           return getWinMesssage(playerSelection, computerSelection);
       }
     case 'Paper':
       switch(computerSelection) {
         case 'Rock':
+          playerScore += 1;
           return getWinMesssage(playerSelection, computerSelection);
         case 'Paper':
           return getTieMessage(playerSelection, computerSelection);
         default:
+          computerScore += 1;
           return getLossMessage(playerSelection, computerSelection);
       }
     case 'Scissors':
       switch(computerSelection) {
         case 'Rock':
+          computerScore += 1;
           return getLossMessage(playerSelection, computerSelection);
         case 'Paper':
+          playerScore += 1;
           return getWinMesssage(playerSelection, computerSelection);
         default:
           return getTieMessage(playerSelection, computerSelection);
@@ -51,12 +57,20 @@ function getOutcome(playerSelection, computerSelection) {
   }
 }
 
+function checkGame() {
+  if (playerScore === 5) alert('You have won the game!');
+  if (computerScore === 5) alert ('You have lost the game!');
+}
+
 function playRound(e) {
   const playerSelection = this.className;
   const computerSelection = getComputerChoice();
 
   const outcome = getOutcome(playerSelection, computerSelection);
   document.getElementById('result').textContent = outcome;
+  document.getElementById('running-score').textContent = `Score: Player ${playerScore} | Computer ${computerScore}`;
+
+  checkGame();
 }
 
 // logic of gameplay here
